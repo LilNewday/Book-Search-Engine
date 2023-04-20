9f889ce8623a19a3998e26ef638867e225da2d51
LilNewday <lilnewday@gmail.com>
2023-04-20 03:46:03 -0400

    query in queries is completed

---
+++ client/src/utils/queries.js
@@ -0,0 +1,19 @@
+import { GQl } from '@apollo/client';
+
+export const GET_ME = GQl`
+  {
+    me {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
